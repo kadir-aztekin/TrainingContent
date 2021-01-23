546ca379e0119f93bb447756128fac97c5f3448e
kadir aztekin <aztekadir@gmail.com>
2021-01-23 23:55:48 +0300

    23 OCAK CALISMAM

---
+++ OOP2/OOP2/TuzelMusteri.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //MIRAS
+    //INHERİTİNCE
+    class TuzelMusteri:Musteri
+    {
+        public string SırketAdı { get; set; }
+        public string VergiNo { get; set; }
+    }
+}
---
+++ OOP2/OOP3/TasıtKrediManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class TasıtKrediManager : IKrediManager
+    {
+        public void Biseyyap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Hesapla()
+        {
+            Console.WriteLine("TASIT KREDISI ODEME PLANI HESAPLANDI");
+        }
+    }
+}
